8f882d2a6c7f6e03933b4cc71884ab5e8cdc4788
HowardLi <l78371@gmail.com>
2025-01-22 15:53:50 +0800

    後端訂單更新

--- backend/cart/urls.py
+++ backend/cart/urls.py
@@ -5,4 +5,6 @@ app_name = 'cart'
 
 urlpatterns = [
     path('', views.order_list, name='orders'),
+    # path('<int:order_id>/edit/', views.edit_order, name='edit_order'),
+    # path('<int:order_id>/delete/', views.delete_order, name='delete_order'),
 ]
--- backend/cart/views.py
+++ backend/cart/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from cart.models import Orders
-from users.models import MemberBasic
+from django.contrib import messages
 
+#獲取訂單
 def order_list(request):
     orders = Orders.objects.all()  # 獲取所有訂單
     return render(request, 'cart/orders_list.html', {'orders': orders})
+
+#編輯訂單
+# def edit_order(request, order_id):
+
+#刪除訂單
+# def delete_order(request, order_id):
+
