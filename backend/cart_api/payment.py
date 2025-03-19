b57276185facf473df410cf6d4b930ef6266cb5c
HowardLi <l78371@gmail.com>
2025-03-19 18:20:38 +0800

    Update payment.py

--- backend/cart_api/payment.py
+++ backend/cart_api/payment.py
@@ -31,7 +31,7 @@ class ECPayPaymentView(APIView):
             "TotalAmount": str(int(order.total_amount)),
             "TradeDesc": "商品支付",
             "ItemName": "訂單商品",
-            "ReturnURL": "https://7793-1-160-29-99.ngrok-free.app/cart_api/ecpay/callback/",
+            "ReturnURL": "https://609d-1-160-3-175.ngrok-free.app/cart_api/ecpay/callback/",
             "ClientBackURL": "http://localhost:5173/orderlist",
             "ChoosePayment": "ALL",
             "EncryptType": 1
